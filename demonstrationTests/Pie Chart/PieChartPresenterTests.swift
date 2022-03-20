5ef3e4744932d3e5a0a2930dc6425b40c684a197
Fernando Lucheti <fernandolucheti@gmail.com>
2022-03-20 20:17:30 -0300

    chore: added tests for Pie Chart scene

---
+++ demonstrationTests/Pie Chart/PieChartPresenterTests.swift
@@ -0,0 +1,68 @@
+//
+//  PieChartPresenterTests.swift
+//  demonstration
+//
+//  Created by Fernando Lucheti on 20/03/22.
+//  Copyright (c) 2022 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+@testable import demonstration
+import XCTest
+import Moya
+
+class PieChartPresenterTests: XCTestCase {
+    
+    lazy var service = PieChartServiceMock()
+    lazy var sut = PieChartPresenter(service: service)
+    lazy var presenterSpy = PieChartPresenterSpy()
+    
+    func testFetch() throws {
+        sut.setDelegate(presenterSpy)
+        sut.fetch()
+        
+        XCTAssertTrue(presenterSpy.viewModel?.categories.first?.name == "Restaurants")
+        XCTAssertTrue(presenterSpy.viewModel?.categories.first?.color == UIColor(hex: "#A00014"))
+        XCTAssertTrue(presenterSpy.viewModel?.categories.first?.totalSpent == 541.5)
+    }
+    
+    func testError() {
+        sut = PieChartPresenter(service: PieChartServiceErrorMock())
+        sut.setDelegate(presenterSpy)
+        sut.fetch()
+        XCTAssertTrue(presenterSpy.error == NetworkErrors.generic)
+    }
+}
+
+final class PieChartServiceMock: PieChartServiceLogic {
+    func fetch(completion: @escaping (Result<PieChartModels.Response, NetworkErrors>) -> Void) {
+        let response = PieChartModels.Response(categories: [
+            PieChartModels.Category(name: "Restaurants", color: "#A00014", totalSpent: 541.5)
+        ])
+        completion(.success(response))
+    }
+}
+
+final class PieChartServiceErrorMock: PieChartServiceLogic {
+    
+    func fetch(completion: @escaping (Result<PieChartModels.Response, NetworkErrors>) -> Void) {
+        completion(.failure(NetworkErrors.generic))
+    }
+}
+
+final class PieChartPresenterSpy: PieChartPresenterDelegate {
+    
+    var presentSuccessCalled = false
+    var presentErrorCalled = false
+    var viewModel: PieChartModels.ViewModel?
+    var error: NetworkErrors?
+    
+    func presentSuccess(viewModel: PieChartModels.ViewModel) {
+        presentSuccessCalled = true
+        self.viewModel = viewModel
+    }
+    
+    func presentError(_ error: NetworkErrors) {
+        presentErrorCalled = false
+        self.error = error
+    }
+}
---
+++ demonstrationTests/Pie Chart/PieChartWorkerTests.swift
@@ -0,0 +1,31 @@
+//
+//  PieChartWorkerTests.swift
+//  demonstration
+//
+//  Created by Fernando Lucheti on 20/03/22.
+//  Copyright (c) 2022 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+@testable import demonstration
+import XCTest
+import Moya
+
+class PieChartServiceTests: XCTestCase {
+    
+    lazy var provider = MoyaProvider<NetworkRequests>(stubClosure: MoyaProvider.immediatelyStub)
+    lazy var sut = PieChartService(provider: provider)
+    
+    func testFetch() {
+        
+        sut.fetch { result in
+            switch result {
+            case .success(let response):
+                XCTAssertTrue(response.categories.first?.name == "Restaurants")
+                XCTAssertTrue(response.categories.first?.color == "#A00014")
+                XCTAssertTrue(response.categories.first?.totalSpent == 541.5)
+            case .failure:
+                XCTFail()
+            }
+        }
+    }
+}
