b4fa75d171abd63821ca50df87092f153bedc929
Fernando Lucheti <fernandolucheti@gmail.com>
2022-03-20 23:07:44 -0300

    chore: add tests to budget bar

---
+++ demonstrationTests/BudgetBar/VerticalTabBarWorkerTests.swift
@@ -0,0 +1,25 @@
+//
+//  VerticalTabBarWorkerTests.swift
+//  demonstration
+//
+//  Created by Fernando Lucheti on 20/03/22.
+//  Copyright (c) 2022 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+@testable import demonstration
+import XCTest
+import Moya
+
+class VerticalTabBarServiceTests: XCTestCase {
+    
+    lazy var provider = MoyaProvider<NetworkRequests>(stubClosure: MoyaProvider.immediatelyStub)
+    lazy var sut = VerticalTabBarService(provider: provider)
+    
+    func testFetch() {
+        
+        sut.fetch { result in
+            XCTAssertTrue(result.categories.first?.color == "#A00014")
+            XCTAssertTrue(result.categories.first?.totalSpent == 541.5)
+        }
+    }
+}
